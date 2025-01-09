ed2003bd95847ddd92283c0d5d6c07a9f41d1de3
nandanlikith <nandanlikith@gmail.com>
2025-01-09 21:57:19 +0530

    making changes in the product and category class

--- src/main/java/com/ecommerce/model/Category.java
+++ src/main/java/com/ecommerce/model/Category.java
@@ -2,6 +2,7 @@ package com.ecommerce.model;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
+    @NotBlank
     private String categoryName;
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
