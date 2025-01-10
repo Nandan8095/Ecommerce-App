549ee51d1bb89b68560c373d6e27bfaf70a8e565
kvHarshith <harshakv79@gmail.com>
2025-01-10 18:21:11 +0530

    Revert "this is an addition of annotation in product"

--- src/main/java/com/ecommerce/model/Product.java
+++ src/main/java/com/ecommerce/model/Product.java
@@ -2,11 +2,9 @@ package com.ecommerce.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 @Entity
 @Data
@@ -23,7 +21,6 @@ public class Product{
     @NotBlank
     private String description;
     private Integer quantity;
-    @NotNull
     private double price;
     private double discount;
     private double specialPrice;
